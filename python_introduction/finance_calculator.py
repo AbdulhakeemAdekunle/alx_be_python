5d482c7f23027a638d3bb4cd27bfbf743a2c09cd
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-06-27 08:51:47 -0700

    renamed all variable and added a typecast operator appropriately

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
 # Get user input for financial details
 
-monthlyIncome = int(input("Enter your monthly income: "))
-monthlyExpenses = int(input("Enter your monthly expenses: "))
-monthlySavings = monthlyIncome - monthlyExpenses
-projectedSavings = monthlySavings * 12 + (monthlySavings * 12 * 0.05)
-print("Your monthly savings are", monthlySavings)
-print("Projected savings after one year, with interest, is: ", projectedSavings)
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = float(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+print("Your monthly savings are", monthly_savings)
+print("Projected savings after one year, with interest, is: ", projected_savings)
