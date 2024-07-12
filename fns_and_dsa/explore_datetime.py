2fd4649b3a02a414d233b4431e88696c764f3e15
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-07-12 02:52:42 -0700

    corrected some sytax errors

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,13 +4,13 @@ from datetime import timedelta
 def display_current_date():
     now = datetime.datetime.now()
     current_date = now.strftime("%Y-%m-%d, %H:%M:%S")
-    print(current_date)
+    print("Current date and time:", current_date)
 display_current_date()
 
 def calculate_future_date():
-    number_of_days = int(input("Enter a number of days: "))
+    number_of_days = int(input("Enter the number of days to add to the current date: "))
     current_date = datetime.date.today()
     number_of_days = timedelta(days=number_of_days)
     future_date = current_date + number_of_days
-    print(future_date)
+    print("Future date:", future_date)
 calculate_future_date()
