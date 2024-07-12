2fc47c2fbf70db2585bb61f3bf1bf280b6f1d41c
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-07-12 03:11:43 -0700

    corrected some sytax errors per checker requirements

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -5,12 +5,12 @@ def display_current_datetime():
     now = datetime.datetime.now()
     current_date = now.strftime("%Y-%m-%d %H:%M:%S")
     print("Current date and time:", current_date)
-display_current_date()
+display_current_datetime()
 
 def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: "))
     current_date = datetime.date.today()
     number_of_days = timedelta(days=number_of_days)
     future_date = current_date + number_of_days
-    print("Future date:", future_date)
+    print("Future date:", future_date.strftime("%Y-%m-%d"))
 calculate_future_date()
