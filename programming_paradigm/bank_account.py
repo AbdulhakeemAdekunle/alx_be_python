4c8620fd1757cbc608e49d36fbe7fb3f81bc1d0e
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-07-17 15:54:32 +0100

    correct a syntax error per checker requirement

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,14 +1,18 @@
 class BankAccount:
-    def __init__(self, account_balance):
-        self.account_balance = 0
-
+    def __init__(self, initial_balance = 0):
+        self.account_balance = initial_balance
+        
     def deposit(self, amount):
         self.amount = amount
-        return self.amount + self.account_balance
+        dp_amount = self.amount + self.account_balance
 
     def withdraw(self, amount):
         self.amount = amount
-        return self.account_balance - self.amount
-
+        if self.amount <= self.account_balance:
+            wd_amount = self.account_balance - self.amount
+            return True
+        else:
+            return False
+        
     def display_balance(self):
         print(f"Current Balance: {self.account_balance}")
