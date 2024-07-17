2fe344930651cc18f94b0bfc296008e1a1587607
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-07-17 17:59:59 +0100

    created a unit test to test my simple_calculator program

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,13 +6,13 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
     
-    def test_add(self):
+    def test_addittion(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-2, 6), 4)
         self.assertEqual(self.calc.add(-3, -5), -8)
         self.assertEqual(self.calc.add(-9, 9), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(3, 1), 2)
         self.assertEqual(self.calc.subtract(-2, 6), -8)
         self.assertEqual(self.calc.subtract(-3, -5), 2)
