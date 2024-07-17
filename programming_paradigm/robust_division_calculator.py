8a668fbb955723a1fec74ccd4cd6b8e95a4bc3c5
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-07-17 16:30:17 +0100

    created a robust division calculator program

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,5 +1,5 @@
 def safe_divide(numerator, denominator):
-    division = float(numerator / denominator)
+    division = float(numerator) / float(denominator)
     return f"The result of the division is {division:.2f}"
 try:
     print(safe_divide(5, 3))
