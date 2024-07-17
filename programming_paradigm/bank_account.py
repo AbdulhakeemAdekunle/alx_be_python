91822e0c030735bf93d64837fcb2770cb2a1bbfb
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-07-17 07:14:33 -0700

    created a simple bank account operation program using class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.amount = amount
+        return self.amount + self.account_balance
+
+    def withdraw(self, amount):
+        self.amount = amount
+        if self.amount <= self.account_balance:
+            return self.account_balance - self.amount
+        else:
+            return "Insufficient balance"
+
+    def display_balance(self):
+      print(f"{self.account_balance}")
